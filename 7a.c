bd29a22030fe137bc8a669d823550e5e48e80eec
Karthikeyan <karthikeyan.36947@gmail.com>
2024-10-08 09:56:36 +0530

    Create 7a.c

---
+++ 7a.c
@@ -0,0 +1,13 @@
+#include <lpc21xx.h>
+
+int main() {
+    int a = 6, b = 2;
+    int sum, sub, mul, div;
+
+    sum = a + b;     // Addition
+    sub = a - b;     // Subtraction
+    mul = a * b;     // Multiplication
+    div = a / b;     // Division
+
+    return 0;
+}
