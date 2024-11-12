8727fe3aad022a365bc449e914170eab382aa12f
Karthikeyan <karthikeyan.36947@gmail.com>
2024-11-12 22:04:02 +0530

    Update 7a.c

--- 7/7a.c
+++ 7/7a.c
@@ -4,10 +4,10 @@ int main() {
     int a = 6, b = 2;
     int sum, sub, mul, div;
 
-    sum = a + b;     // Addition
-    sub = a - b;     // Subtraction
-    mul = a * b;     // Multiplication
-    div = a / b;     // Division
+    sum = a + b;     
+    sub = a - b;     
+    mul = a * b;     
+    div = a / b;     
 
     return 0;
 }
